f0fae1e88848e2d6df0d32c31d02f5dd8ec5bc3e
pendolf1984 <70535199+pendolf1984@users.noreply.github.com>
2022-12-13 10:54:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ date_start = datetime.datetime.today().replace(hour=0, minute=1, second=0, micro
 date_yesterday = date_start - timedelta(days=1)
 date_end = datetime.datetime.today().replace(hour=23, minute=59, second=0, microsecond=0)
 
-filepath = "D:\\certs\\time.xlsx"
+filepath = ""
 
 server = smtplib.SMTP('mail.1mf.ru', 25)
 
